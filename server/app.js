dac6657ba495266786854d8aaffb42542f172145
Se-Hun <tpgns0539@naver.com>
2019-12-04 14:52:08 +0900

    Add DB Connection code And API Logging code

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const logger = require('morgan')
+// const session = require('express-session')
+
+// For distribution Port
+const API_PORT = 5000;
+
+// Init Express Server App
 const app = express();
 const api = require('./routes/index');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(logger('dev')); // logging
+
 app.use('/api', api);
 
-const port = process.env.PORT || 5000;
+app.get('/test', (req, res) => {
+    return res.send('Hello World');
+})
+
+
+const port = process.env.PORT || API_PORT;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
