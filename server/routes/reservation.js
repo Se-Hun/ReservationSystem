d0bfee00a47c246a58737d9db0114f773833d2e4
potion <potionkr@gmail.com>
2019-12-07 17:03:10 +0900

    add exception handling in reservation.js

--- server/routes/reservation.js
+++ server/routes/reservation.js
@@ -20,25 +20,136 @@ const router = express.Router();
 // output : _ID
 // todo : 반환되는 reservation id를 유저 데이터에다가도 추가해줘야함
 router.post('/reserve', (req, res) => {
+    const departure = req.body.departure;
+    const arrival = req.body.arrival;
+    const date = req.body.date;
+    const time = req.body.time;
+    const peoplenum = req.body.peoplenum;
+    const age = req.body.age;
+    const way = req.body.way;
+    const card = req.body.card;
+    const cardnum = req.body.cardnum;
+    const state = req.body.state;
+
+    if(!departure)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "departure"});
+    if(!arrival)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "arrival"});     
+    if(!date)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "date"});
+    if(!time)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "time"});   
+    if(!peoplenum)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "peoplenum"});
+    if(!age)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "age"});
+    if(!way)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "way"});   
+    if(!card)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "card"});
+    if(!cardnum)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "cardnum"});
+    if(!state)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "state"});
+
     Reservation.reserve(req.body)
     .then(reservation => res.send(reservation._id))
-    .catch(err => res.status(500).send(err));
+    .catch(err => res.status(500).send({code: '500', error: 3, state: false}));
 })
 
 // input : 해당 예매 정보에 대한 id
-// output :  _ID
+// output : state
 router.post('/deleteReservation', (req, res) => {
-    Reservation.deleteReservation(req.body.id)
-    .then(reservation => res.send(reservation._id))
-    .catch(err => res.status(500).send(err));
+    const id = req.body.id;
+    if(!id)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "id"});
+    Reservation.deleteReservation(id)
+    .then((reservation) => {
+        if(!reservation) {
+            // 없을 경우
+            return res.status(404).send({code: '404', error: 2, state: false})
+        }
+        else {
+            res.send({state: true});
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({code: '500', error: 3, state: false})
+    })
 })
 
 // input : 해당 예매 정보에 대한 id
-// output : _ID
+// output :  해당 예매 정보
+router.post('/getReservation', (req, res) => {
+    const id = req.body.id;
+    if(!id)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "id"});
+    Reservation.getReservation(id)
+    .then((reservation) => {
+        if(!reservation) {
+            // 없을 경우
+            return res.status(404).send({code: '404', error: 2})
+        }
+        else {
+            res.send(reservation);
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({code: '500', error: 3})
+    })
+})
+
+// input: 아래 예시와 같은 형식의 데이터 
+/*
+{
+    "departure": "daejeon",
+    "arrival": "seoul",
+    "date": "2019-12-11",
+    "time": "09:00",
+    "peoplenum": "1",
+    "age": "children",
+    "way": "oneway",
+    "card": "Kakao_bank",
+    "cardnum": "3333-33-3333",
+    "state": "0"
+}
+*/
+// output : state
 router.post('/editReservation', (req, res) => {
-    Reservation.editReservation(req.body.id, req.body.departure, req.body.arrival, req.body.date, req.body.time, req.body.peoplenum, req.body.age, req.body.way, req.body.card, req.body.cardnum, req.body.state)
-    .then(reservation => res.send(reservation._id))
-    .catch(err => res.status(500).send(err));
+    const departure = req.body.departure;
+    const arrival = req.body.arrival;
+    const date = req.body.date;
+    const time = req.body.time;
+    const peoplenum = req.body.peoplenum;
+    const age = req.body.age;
+    const way = req.body.way;
+    const card = req.body.card;
+    const cardnum = req.body.cardnum;
+    const state = req.body.state;
+
+    if(!departure)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "departure"});
+    if(!arrival)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "arrival"});     
+    if(!date)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "date"});
+    if(!time)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "time"});   
+    if(!peoplenum)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "peoplenum"});
+    if(!age)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "age"});
+    if(!way)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "way"});   
+    if(!card)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "card"});
+    if(!cardnum)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "cardnum"});
+    if(!state)
+        return res.status(404).send({code: '404', error: 1, shouldAttribute: "state"});
+    Reservation.editReservation(req.body)
+    .then(res.send({state: true}))
+    .catch(res.status(500).send({code: '500', error: 3, state: false}));
 })
 
 module.exports = router
