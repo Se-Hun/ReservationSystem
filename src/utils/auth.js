a96ccbccf7d56d88546ae5b900f5d74a50e1817a
someonewho <bluedao2@naver.com>
2019-12-13 03:57:22 +0900

    password error

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,22 +9,17 @@ export function deleteTokens() {
     sessionStorage.removeItem("phonenum")
     sessionStorage.removeItem("cardcompany")
     sessionStorage.removeItem("cardnum")
-    sessionStorage.removeItem("password")
 }
 
-export function login(account, token, accountname, phonenum, cardcompany, cardnum, password) {
+export function login(account, token, accountname, phonenum, cardcompany, cardnum) {
     sessionStorage.setItem("account", account)
     sessionStorage.setItem("token", token)
     sessionStorage.setItem("accountname", accountname)
     sessionStorage.setItem("phonenum", phonenum)
     sessionStorage.setItem("cardcompany", cardcompany)
     sessionStorage.setItem("cardnum", cardnum)
-    sessionStorage.setItem("password", password)
 }
 
 export function getId() {
     sessionStorage.getItem("account")
-}
-export function getPassword()   {
-    sessionStorage.getItem("password")
 }
